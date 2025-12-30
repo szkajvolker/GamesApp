380a7d570bd08c518dd89b5f4141218f9d3ea368
szkajvolker <ist9595@windowslive.com>
2025-12-30 22:11:14 +0100

    backend routes

--- backend/routes/rawg.js
+++ backend/routes/rawg.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get("/genre", getGameByGenre);
 router.get("/platform", getGameByPlatform);
 router.get("/", getGames);
-router.get("/:id", getGameById);
 router.get("/featured", getFeaturedGames);
+router.get("/:id", getGameById);
 
 export default router;
