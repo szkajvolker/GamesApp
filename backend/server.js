3d7be542c0299941341288bf7c732bb622db147c
szkajvolker <ist9595@windowslive.com>
2026-01-02 15:36:40 +0100

    add CORS

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import rawgRoutes from "./routes/rawg.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3333;
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://games-store-db.netlify.app"],
+  })
+);
+
 app.use(express.json());
 app.use("/api/games", rawgRoutes);
-//TODO implement cors
-//app.use(cors())
 
 app.listen(PORT, () => {
   connectDB();
