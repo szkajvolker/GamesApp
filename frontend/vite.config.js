0d426762a5d6053d6591587ca56c937624855596
szkajvolker <ist9595@windowslive.com>
2026-02-24 18:07:30 +0100

    removed manual chunks

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,28 +10,6 @@ export default defineConfig({
     },
   },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (
-              id.includes("react") ||
-              id.includes("react-dom") ||
-              id.includes("react-router-dom")
-            ) {
-              return "react-vendor";
-            }
-            if (id.includes("framer-motion") || id.includes("gsap")) {
-              return "animation-vendor";
-            }
-            if (id.includes("@fortawesome") || id.includes("react-icons")) {
-              return "icons-vendor";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
     chunkSizeWarningLimit: 1000,
   },
 });
